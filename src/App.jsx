ce068bf262349dece563ee3c4c2131ac4f2af602
Greg Hayes <gthelight@gmail.com>
2022-10-13 16:27:31 -0600

    config and setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./styles";
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <div className="bg-primary overflow-hidden w-full">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>NavBar</div>
+    </div>
+  </div>
+);
 
 export default App;
